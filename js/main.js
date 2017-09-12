73a6f315eaef5ea4fef67b96876a84d948ed4a88
Seastream <albin.seastream@gmail.com>
2017-09-12 13:26:24 +0200

    Shooting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var player;
-var speed = 5;
+var moveSpeed = 5;
+var bulletSpeed = 10;
+var bullets = [];
+var mousePressed = false;
 
 function preload() {
 	game.load.image('player', 'assets/player.png');
 	game.load.image('weapon', 'assets/weapon.png');
+	game.load.image('bullet1', 'assets/bullet.png');
+	game.load.image('bullet2', 'assets/bullet.png');
 }
 
 function create() {
@@ -20,11 +25,42 @@ function create() {
 	aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
 	sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
 	dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+	LMB = game.input.activePointer;
 }
 
 function update() {
 	movePlayer();
 	aim();
+	shoot();
+}
+
+function shoot() {
+	if (LMB.isDown && !mousePressed) {
+		mousePressed = true;
+		var bullet;
+		if (Math.random() < 0.5) {
+			bullet = game.add.sprite(player.x, player.y, 'bullet1');
+		}
+		else {
+			bullet = game.add.sprite(player.x, player.y, 'bullet2');
+		}
+		bullet.anchor.setTo(0.5, 0.5);
+		bullet.angle = playerRadians;
+		bullets.push(bullet);
+	}
+	else if (!LMB.isDown && mousePressed) {
+		mousePressed = false;
+	}
+	
+	for (i = 0; i < bullets.length; i++) {
+		bullets[i].x += bulletSpeed * Math.cos(bullets[i].angle);
+		bullets[i].y += bulletSpeed * Math.sin(bullets[i].angle);
+		
+		if (!bullets[i].inWorld) {
+			bullets.splice(i, 1);
+		}
+	}
+	console.log(bullets.length);
 }
 
 function movePlayer() {
@@ -32,16 +68,16 @@ function movePlayer() {
 	moveY = 0;
 	
 	if (wKey.isDown) {
-		moveY -= speed;
+		moveY -= moveSpeed;
 	}
 	if (aKey.isDown) {
-		moveX -= speed;
+		moveX -= moveSpeed;
 	}
 	if (sKey.isDown) {
-		moveY += speed;
+		moveY += moveSpeed;
 	}
 	if (dKey.isDown) {
-		moveX += speed;
+		moveX += moveSpeed;
 	}
 	
 	if (moveX != 0 && moveY != 0) {
@@ -60,13 +96,14 @@ function aim() {
 	mouseX = game.input.mousePointer.x;
 	mouseY = game.input.mousePointer.y;
 	
-	radians = Math.atan(-(player.y - mouseY) / (mouseX - player.x));
-	degrees = radians * (360 / (2 * Math.PI));
+	playerRadians = Math.atan(-(player.y - mouseY) / (mouseX - player.x));
+	playerDegrees = playerRadians * (360 / (2 * Math.PI));
 	
 	if (mouseX < player.x) {
-		degrees += 180;
+		playerDegrees += 180;
+		playerRadians += Math.PI;
 	}
 	
-	player.angle = degrees;
-	weapon.angle = degrees;
+	player.angle = playerDegrees;
+	weapon.angle = playerDegrees;
 }
