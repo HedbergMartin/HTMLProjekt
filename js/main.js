22906dbb18ee8dd2b17053c5a6ca06ea11e4181d
Martin Hedberg <96mah002@gmail.com>
2017-09-13 23:07:27 +0200

    Implemented phasers physics system and added 'bugs'. Borde ha gjort fler commits längst vägen :)

--- js/main.js
+++ js/main.js
@@ -1,63 +1,84 @@
 var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var player;
-var moveSpeed = 5;
-var bulletSpeed = 10;
-var bullets = [];
+var moveSpeed = 300;
+var bulletSpeed = 900;
+var bullets;
 var mousePressed = false;
+
+var bugs;
+var bugMoveSpeed = 200;
 
 function preload() {
 	game.load.image('player', 'assets/player.png');
 	game.load.image('weapon', 'assets/weapon.png');
-	game.load.image('bullet1', 'assets/bullet.png');
-	game.load.image('bullet2', 'assets/bullet.png');
+	game.load.image('bullet1', 'assets/0.png');
+	game.load.image('bullet2', 'assets/1.png');
+	game.load.image('bug1', 'assets/bug1.png');
 }
 
 function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
 	player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+	game.physics.enable(player, Phaser.Physics.ARCADE);
+	player.body.collideWorldBounds = true;
 	player.anchor.setTo(0.5, 0.5);
 	
-	weapon = game.add.sprite(0, 0, 'weapon');
-	weapon.anchor.setTo(0, -1);
+	//weapon = game.add.sprite(0, 0, 'weapon');
+	//weapon.anchor.setTo(0, -1);
+	bullets = game.add.group();
+	bullets.enableBody = true;
+	bullets.physicsBodyType = Phaser.Physics.ARCADE;
+	bullets.createMultiple(20, ['bullet1', 'bullet2']);
+	bullets.setAll('outOfBoundsKill', true);
+	bullets.setAll('checkWorldBounds', true);
+	bullets.setAll('anchor.x', .5);
+	bullets.setAll('anchor.y', .5);
+	bullets.setAll('scale.x', .5);
+	bullets.setAll('scale.y', .5);
+
+	bugs = game.add.group();
+	bugs.enableBody = true;
+	bugs.physicsBodyType = Phaser.Physics.ARCADE;
 	
 	wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 	aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
 	sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
 	dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
 	LMB = game.input.activePointer;
+
+	spawnBugs(10); //TODO remove and implement waves
 }
 
 function update() {
 	movePlayer();
 	aim();
 	shoot();
+
+	//updateBugs();
+	game.physics.arcade.overlap(bullets, bugs, collisionHandler, null, this);
+	game.physics.arcade.collide(bugs, bugs);
+	bugs.forEach(updateBug, this, true);
+}
+
+function updateBug(bug) {
+	var angle = game.physics.arcade.moveToObject(bug, player, bugMoveSpeed);
+	bug.angle = angle * (360 / (2 * Math.PI));
 }
 
 function shoot() {
 	if (LMB.isDown && !mousePressed) {
 		mousePressed = true;
-		var bullet;
-		if (Math.random() < 0.5) {
-			bullet = game.add.sprite(player.x, player.y, 'bullet1');
+		bullet = getRandomNonExists(bullets);
+		if (bullet) {
+			bullet.reset(player.x, player.y);
+			bullet.body.velocity.x = bulletSpeed * Math.cos(playerRadians);
+			bullet.body.velocity.y = bulletSpeed * Math.sin(playerRadians);
 		}
-		else {
-			bullet = game.add.sprite(player.x, player.y, 'bullet2');
-		}
-		bullet.anchor.setTo(0.5, 0.5);
-		bullet.angle = playerRadians;
-		bullets.push(bullet);
 	}
 	else if (!LMB.isDown && mousePressed) {
 		mousePressed = false;
-	}
-	
-	for (i = 0; i < bullets.length; i++) {
-		bullets[i].x += bulletSpeed * Math.cos(bullets[i].angle);
-		bullets[i].y += bulletSpeed * Math.sin(bullets[i].angle);
-		
-		if (!bullets[i].inWorld) {
-			bullets.splice(i, 1);
-		}
 	}
 }
 
@@ -84,27 +105,13 @@ function movePlayer() {
 		moveY *= Math.cos(Math.PI/4);
 	}
 	
-	player.x += moveX;
-	player.y += moveY;
-	
-	if (player.x + player.width / 2 > game.world.width) {
-		player.x = game.world.width - player.width / 2;
-	}
-	else if (player.x - player.width / 2 < 0) {
-		player.x = player.width / 2;
-	}
-	if (player.y + player.height / 2 > game.world.height) {
-		player.y = game.world.height - player.height / 2;
-	}
-	else if (player.y - player.height / 2 < 0) {
-		player.y = player.height / 2;
-	}
-	
+	player.body.velocity.x = moveX;
+	player.body.velocity.y = moveY;
 }
 
 function aim() {
-	weapon.x = player.x;
-	weapon.y = player.y;
+	//weapon.x = player.x;
+	//weapon.y = player.y;
 	
 	mouseX = game.input.mousePointer.x;
 	mouseY = game.input.mousePointer.y;
@@ -118,5 +125,53 @@ function aim() {
 	}
 	
 	player.angle = playerDegrees;
-	weapon.angle = playerDegrees;
+	//weapon.angle = playerDegrees;
 }
+
+function spawnBugs(amount) {
+	for (var i = 0; i < amount; i++) {
+		var spawnX, spawnY;
+		switch (game.math.between(0,3)){ //TODO byt till världsbredd
+			case 0: //Upper wall
+				spawnX = (Math.random() * 768) + 16;
+				spawnY = 16;
+			break;
+			case 1: //Right wall
+				spawnX = 784;
+				spawnY = (Math.random() * 768) + 16;
+			break;
+			case 2: //Bottom wall
+				spawnX = (Math.random() * 768) + 16;
+				spawnY = 784;
+			break;
+			case 3: //Left wall
+				spawnX = 16;
+				spawnY = (Math.random() * 768) + 16;
+			break;
+		};
+
+		bugs.create(spawnX, spawnY, 'bug1');
+	}
+}
+
+function updateBugs() {
+	for (var i = 0; i < bugs.length; i++) {
+		var bugRadians = Math.atan(-(bugs[i].y - player.y) / (player.x - bugs[i].x));
+		bugs[i].angle = bugRadians * (360 / (2 * Math.PI));
+		
+		if (player.x < bugs[i].x) {
+			bugs[i].angle += 180;
+			bugRadians += Math.PI;
+		}
+	}
+}
+
+function collisionHandler(bullet, bug) {
+	bullet.kill();
+	bug.kill();
+}
+
+function getRandomNonExists(group) {
+    var list = group.getAll('exists', false);
+    return game.rnd.pick(list);
+};
